bdc8cfa4f2fa432f2069b79ef60ba8839f657580
amirgo <amirgo@stanford.edu>
2021-09-08 17:36:16 -0700

    Update generate_network.m

--- generate_network.m
+++ generate_network.m
@@ -13,24 +13,4 @@ if strcmp(type,'caveman')
 elseif strcmp(type,'full')
     S = ones(length(groups));
     S = S-eye(size(S));
-elseif strcmp(type,'scalefree')
-    if beta==-1
-        beta = 1;
-    end
-    p = exp(beta*(1:length(groups)));
-    p = p./sum(p);
-    alpha = 0;
-    while alpha<2 || alpha>3
-        S = zeros(length(groups));
-        for i=1:length(groups)
-            p_ = p;
-            p_(i) = 0;
-            for j=1:6
-                q = discreteinvrnd(p_);
-                S(i,q) = 1;
-                p_(q) = 0;
-            end
-        end
-        [alpha]=powerlaw_fit(sum(S),'finite');
-    end
 end
